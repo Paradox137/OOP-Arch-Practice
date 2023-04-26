3639c76012cd8b028933138ccc6c7c27994f902b
Igor X <iverbitskiy321@gmail.com>
2023-04-26 11:28:22 +0300

    add CooldownComponent

---
+++ Asteroids2D/Assets/Code/Framework/Entities/Components/CooldownComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Asteroids.Framework.Entities.ContractsComponent;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Asteroids.Framework.Entities.Components
+{
+	public struct CooldownComponent : ICooldownable
+	{
+		public readonly float TimeToWait { get;}
+		public float TimeSpend { get; private set; }
+		
+		public event Action onTimerEnd;
+
+		public CooldownComponent(float timeToWait)
+		{
+			TimeSpend = 0f;
+			this.TimeToWait = timeToWait;
+			onTimerEnd = null;
+		}
+		
+		public async void StartTimer()
+		{
+			TimeSpend = 0f;
+			
+			await TimerTickPerDeltaTime();
+			
+			onTimerEnd?.Invoke();
+		}
+
+		private async UniTask TimerTickPerDeltaTime()
+		{
+			while (Math.Abs(TimeSpend - TimeToWait) > 0.001f)
+			{
+				TimeSpend += Time.deltaTime;
+				await UniTask.NextFrame();
+			}
+		}
+	}
+}
---
+++ Asteroids2D/Assets/Code/Framework/Entities/ContractsComponent/ICooldownable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Asteroids.Framework.Entities.ContractsComponent
+{
+	public interface ICooldownable
+	{
+		float TimeToWait { get; }
+
+		float TimeSpend { get; }
+
+		event Action onTimerEnd;
+
+		void StartTimer();
+	}
+}
